0b80254d05329f352c19242310170c01dcf56f7e
Nikola Nikolov <nnickoloff@gmail.com>
2022-04-04 17:59:29 +0300

    auth

---
+++ routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/**
+ * Buyers
+ */
+Route::resource('buyers','App\Http\Controllers\Buyer\BuyerController',['only' => ['index','show']]);
+/**
+ * Categories
+ */
+Route::resource('categories','App\Http\Controllers\Category\CategoryController',['except' => ['create','edit']]);
+/**
+ * Products
+ */
+Route::resource('products','App\Http\Controllers\Product\ProductController',['only' => ['index','show']]);
+/**
+ * Sellers
+ */
+Route::resource('sellers','App\Http\Controllers\Seller\SellerController',['only' => ['index','show']]);
+/**
+ * Transactions
+ */
+Route::resource('transactions','App\Http\Controllers\Transaction\TransactionController',['only' => ['index','show']]);
+/**
+ * Users
+ */
+Route::resource('users','App\Http\Controllers\User\UserController',['except' => ['create','edit']]);
