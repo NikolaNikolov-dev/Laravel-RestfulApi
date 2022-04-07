6667e15522f0251431de541c7f7d42fe50f11f1d
Nikola Nikolov <nnickoloff@gmail.com>
2022-04-07 13:45:01 +0300

    ApiResponser and ApiController created

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\Buyer;
 
 use App\Models\Buyer;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use Illuminate\Foundation\Auth\User;
 
-class BuyerController extends Controller
+class BuyerController extends ApiController
 {
     /**
      * Display a listing of the resource.
