4fa0adbc2f36fe2f9825b31994212f467a4fc000
Nikola Nikolov <nnickoloff@gmail.com>
2022-04-11 13:00:28 +0300

    Products Controllers

---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories;
+
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product, Category $category)
+    {
+        // To interract with many to many relationship we use attach,sync,syncWithoutDetach
+        $product->categories()->syncWithoutDetaching([$category->id]);
+
+        return $this->showAll($product->categories);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product,Category $category)
+    {
+        if(!$product->categories()->find($category->id)){
+            return $this->errorResponse('The specified category is not a category of this product',404);
+        }
+
+        $product->categories()->detach($category->id);
+
+        return $this->showAll($product->categories);
+    }
+}
